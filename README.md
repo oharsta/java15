fcbe5778a5c5da4ba8c4ac0fa4a6a08252b49597
Okke Harsta <oharsta@zilverline.com>
2023-11-17 13:48:10 +0100

    Ported to jdk21

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-### Playground for JDK everything after 8 (up till 17)
+### Playground for JDK everything after 8 (up till 21)
 
 ```
 rm -fr ~/.mavenrc
-export JAVA_HOME=/Library/Java/JavaVirtualMachines/openjdk-17.jdk
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/openjdk-21.jdk
 ```
--- src/test/java/zilverline/ControllerTests.java
+++ src/test/java/zilverline/ControllerTests.java
@@ -6,7 +6,7 @@ import io.restassured.http.ContentType;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.boot.test.web.server.LocalServerPort;
 
 import java.util.List;
 import java.util.Map;
